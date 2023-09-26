cc9ae2d5bca385b59d52eacb7147e3bb9050853e
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-09-26 15:17:46 -0400

    refactor: move function binary_search to own file

--- main.c
+++ main.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
-#include <math.h>
-
-int binary_search(int array[], int length, int targetValue)
-{
-  int begin = 0;
-  int end = length - 1;
-
-  while (begin <= end)
-  {
-    int middle = floor((begin + end) / 2);
-    if (array[middle] == targetValue)
-      return middle;
-    else if (targetValue < array[middle])
-      end = middle - 1;
-    else
-      begin = middle + 1;
-  }
-  return -1;
-}
+#include "binary_search.h"
 int main()
 {
   int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
